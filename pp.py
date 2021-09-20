0c269fe28f05f6210629f56a6ae2f9fafe1bf93e
lemo <lemo@qq.com>
2021-09-21 04:42:07 +0800

    修改pp1

--- pp.py
+++ pp.py
@@ -1,26 +1,2 @@
-TransactionHistoryFG.FROM_TXN_DATE: 01/08/2021
-TransactionHistoryFG.TO_TXN_DATE: 15/08/2021
-TransactionHistoryFG.ORDER_BY: DSC
-TransactionHistoryFG.FROM_AMOUNT: 
-TransactionHistoryFG.TO_AMOUNT: 
-TransactionHistoryFG.FROM_INSTRUMENTID: 
-TransactionHistoryFG.TO_INSTRUMENTID: 
-ScreenNameWF: OpAccountDetailsRetUX4
-FG_BUTTONS__: TEMPLATE_ACTION
-TransactionHistoryFG.CONTROLIDLISTING: TransactionListing
-TransactionHistoryFG.TEMPLATE_ACTION_TYPE: Save Template
-TransactionHistoryFG.DATE_FORMAT: defefe
-TransactionHistoryFG.MAPNAME: OpTransactionListing
-TransactionHistoryFG.INITIATOR_ACCOUNT: 071705001000
-TransactionHistoryFG.CLICKED_PAGE_NO: 
-GROUPLET_FORMSGROUP_ID__: TransactionHistoryFG
-TransactionHistoryFG.REPORTTITLE: OpAccountDetailsRetUX4
-RIA_TARGETS: TransactionHistoryFG.PERIOD,,,,
-JS_ENABLED_FLAG: Y
-DECRYPT_FLAG: N
-CHECKBOX_NAMES__: TransactionHistoryFG.AMOUNT_TYPE_CREDIT,TransactionHistoryFG.AMOUNT_TYPE_DEBIT
-Requestid: 3
-Action.SEARCH: Get Details
-__GROUPLET_NAME__: PageConfigurationMaster_CDESTW__1
-__RIA__: GROUPLET
-GROUPLETS_IN_PAGE: ,PageConfigurationMaster_CDESTW__1
+a = 1
+print(a+2)
